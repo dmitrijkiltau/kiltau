54376fb0439ab9be625effbf7e99c96e50659ca2
Dima <dmitrij@kiltau.com>
2021-09-25 22:05:13 +0200

    update column sizes

--- src/components/column.js
+++ src/components/column.js
@@ -4,31 +4,30 @@ import styled from 'styled-components'
 const StyledColumn = styled.div`
   display: flex;
   flex-direction: column;
-  flex: 0 0 calc(100% / ${(props) => props.desktop || 1} - 2rem);
+  flex: 0 0 calc(100% / ${(props) => props.lg || 1} - 2rem);
   align-items: ${(props) => props.alignItems || 'flex-start'};
   justify-content: ${(props) => props.justifyContent || 'flex-start'};
 
   @media (max-width: 61rem) {
-    flex: 0 0 calc(100% / ${(props) => props.tablet || 1} - 2rem);
+    flex: 0 0 calc(100% / ${(props) => props.md || props.lg || 1} - 2rem);
+  }
+
+  @media (max-width: 48rem) {
+    flex: 0 0
+      calc(100% / ${(props) => props.sm || props.md || props.lg || 1} - 2rem);
   }
 
   @media (max-width: 36rem) {
-    flex: 0 0 calc(100% / ${(props) => props.mobile || 1} - 2rem);
+    flex: 0 0 calc(100% / ${(props) => props.xs || 1} - 2rem);
   }
 `
 
-const Column = ({
-  desktop,
-  tablet,
-  mobile,
-  alignItems,
-  justifyContent,
-  children,
-}) => (
+const Column = ({ lg, md, sm, xs, alignItems, justifyContent, children }) => (
   <StyledColumn
-    desktop={desktop}
-    tablet={tablet}
-    mobile={mobile}
+    lg={lg}
+    md={md}
+    sm={sm}
+    xs={xs}
     alignItems={alignItems}
     justifyContent={justifyContent}
   >
