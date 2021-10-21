be51c580330cde26fbccbebb8592fb52b8895444
Dima <dmitrij@kiltau.com>
2021-10-21 22:57:51 +0200

    add animated result component and animate percentage calculator tool

--- src/components/tools/percentage.js
+++ src/components/tools/percentage.js
@@ -1,18 +1,16 @@
-import React, { useRef, useCallback, useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import styled from 'styled-components'
 import { Trans } from 'react-i18next'
 
 import Row from '../row'
 import Column from '../column'
+import AnimatedResult from '../animated-result'
 
-const Tool = styled.div`
+const Tool = styled.form`
+  display: flex;
+  flex-direction: column;
   margin: 1rem 0;
-
-  form {
-    display: flex;
-    flex-direction: column;
-    gap: 2rem;
-  }
+  gap: 2rem;
 `
 
 const Input = styled.input`
@@ -25,67 +23,52 @@ const Input = styled.input`
   }
 `
 
-const Result = styled(Input)`
-  height: 100%;
-  font-size: 1.75rem;
-  text-align: center;
-  box-shadow: 0 2px var(--color-accent-75);
-`
-
 const Percentage = () => {
-  const value1Ref = useRef()
-  const value2Ref = useRef()
-  const resultRef = useRef()
+  const [value1, setValue1] = useState(25)
+  const [value2, setValue2] = useState(100)
+  const [result, setResult] = useState(0)
 
-  const handleChange = useCallback((e) => {
-    e?.preventDefault()
+  const handleChangeValue1 = ({ target: { value } }) => {
+    setValue1(value)
+    handleChangeResult()
+  }
 
-    const value1 = parseFloat(value1Ref.current.value)
-    const value2 = parseFloat(value2Ref.current.value)
+  const handleChangeValue2 = ({ target: { value } }) => {
+    setValue2(value)
+    handleChangeResult()
+  }
 
-    resultRef.current.value =
-      parseFloat(((value1 / value2) * 100).toFixed(2)) + ' %'
-  }, [value1Ref, value2Ref, resultRef])
+  const handleChangeResult = () => {
+    setResult((value1 / value2) * 100)
+  }
 
-  useEffect(handleChange)
+  const formatValue = (value) => `${value.toFixed(2)} %`
+
+  useEffect(handleChangeResult)
 
   return (
     <Tool>
-      <form onSubmit={(e) => handleChange(e)}>
-        <Row>
-          <Column lg={2} xs={2}>
-            <div style={{ marginBottom: '1rem' }}>
-              <Input
-                type="number"
-                ref={value1Ref}
-                name="value1"
-                defaultValue="25"
-                onChange={handleChange}
-              />
-            </div>
+      <Row>
+        <Column lg={2} xs={2}>
+          <div style={{ marginBottom: '1rem' }}>
+            <Input type="number" value={value1} onChange={handleChangeValue1} />
+          </div>
 
-            <div>
-              <label htmlFor="value">
-                <Trans>of</Trans>
-              </label>
-              <Input
-                type="number"
-                ref={value2Ref}
-                name="value2"
-                defaultValue="100"
-                onChange={handleChange}
-              />
-            </div>
-          </Column>
-
-          <Column lg={2} xs={2}>
-            <label htmlFor="result">
-              <Trans>is</Trans>
+          <div>
+            <label htmlFor="value">
+              <Trans>of</Trans>
             </label>
-            <Result ref={resultRef} name="result" disabled />
-          </Column>
-        </Row>
-      </form>
+            <Input type="number" value={value2} onChange={handleChangeValue2} />
+          </div>
+        </Column>
+
+        <Column lg={2} xs={2}>
+          <label htmlFor="result">
+            <Trans>is</Trans>
+          </label>
+          <AnimatedResult value={result} formatValue={formatValue} />
+        </Column>
+      </Row>
     </Tool>
   )
 }
